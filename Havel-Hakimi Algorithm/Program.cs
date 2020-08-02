b2f7ef24a161ee41ea9084cb2a172726b818cc63
NuclearToast <daxter1337@gmx.ch>
2020-08-02 04:13:49 +0200

    Havel-Hakimi: Some cleanup and trying some things to sort the list

--- Havel-Hakimi Algorithm/Program.cs
+++ Havel-Hakimi Algorithm/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,34 +14,52 @@ namespace Havel_Hakimi_Algorithm
         static void Main(string[] args)
         {
             List<int> integers = new List<int>() { 1, 2, 0, 4, 2, 0, 9, 4 };
-            List<int> cleanedIntegers;
-            string writeString = "";
 
-            foreach (int integer in integers)
-            {
-                writeString += integer + ", ";
-            }
-            Console.WriteLine("Original sequence: ");
-            Console.WriteLine("{ " + writeString + " }");
-            Console.WriteLine("\n");
-            writeString = "";
-
-            cleanedIntegers = removeZeroes(integers);
-            foreach (int integer in integers)
-            {
-                writeString += integer + ", ";
-            }
-            Console.WriteLine("Sequence with 0s removed: ");
-            Console.WriteLine("{ " + writeString + " }");
-
+            integers.Sor
+            WriteSequence(ComposeSequenceString(integers));
+            WriteSequence(ComposeSequenceString(RemoveZeroes(integers)), true);
             Console.ReadLine();
         }
 
-        static public List<int> removeZeroes(List<int> input)
+        static public List<int> RemoveZeroes(List<int> input)
         {
             while (input.IndexOf(0) > -1){ input.Remove(0);}
             return input;
         }
-            
+          
+        static public string ComposeSequenceString (List<int> sequences)
+        {
+            string writeString = "";
+            int i = 0;
+            foreach (int integer in sequences)
+            {
+                if (i < sequences.Count -1 )
+                {
+                    writeString += integer + ", ";
+                }
+                else
+                {
+                    writeString += integer;
+                }
+                i++;
+             }
+
+            return writeString;
+        }
+        
+        static public void WriteSequence(string sequence, bool isProcessed = false)
+        {
+            if (isProcessed)
+            {
+                Console.WriteLine("Sequence with 0s removed: ");
+                Console.WriteLine("{ " + sequence + " }");
+            }
+            else
+            {
+                Console.WriteLine("Original sequence: ");
+                Console.WriteLine("{ " + sequence + " }");
+                Console.WriteLine("\n");
+            }
+        }
     }
 }
