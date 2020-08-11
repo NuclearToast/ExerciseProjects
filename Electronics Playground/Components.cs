70f389d1523387908031aae75c10fdece4024300
NuclearToast <daxter1337@gmx.ch>
2020-08-11 19:05:43 +0200

    Tried to emulate a bit

---
+++ Electronics Playground/Components.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Electronics_Playground
+{
+    public class Components
+    {
+        public struct Bit
+        {
+            public Bit(bool state)
+            {
+                bitState = state ? 1 : 0;
+            }
+
+            public static implicit operator Bit(bool val)
+            {
+                Bit x = new Bit(val);
+                return x;
+            }
+            
+            public static implicit operator Bit(int val)
+            {
+                Bit x = new Bit();
+                if (val >-1 && val <2){x.bitState = val;} else { x.bitState = 0; }
+                return x;
+            }
+
+            private int bitState { get; set; }
+
+            public override string ToString() => $"{bitState}";
+        }
+    }
+}
---
+++ Electronics Playground/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Electronics_Playground
+{
+    class Program
+    {
+        public static Components.Bit bit1 = new Components.Bit(true);
+        static void Main(string[] args)
+        {
+            //bit1 = 2;
+            //Console.WriteLine(bit1);
+            Console.WriteLine(0b01);
+            Console.ReadLine();
+        }
+
+    }
+
+
+}
